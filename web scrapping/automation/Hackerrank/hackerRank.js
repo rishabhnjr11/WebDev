7a68e0cdfc3ec77a886c9b28d852d1f1096b6181
Rishabh Verma <rishabhnjr11@gmail.com>
2022-04-14 23:06:55 +0530

    make little tweaks progress

--- web scrapping/automation/Hackerrank/hackerRank.js
+++ web scrapping/automation/Hackerrank/hackerRank.js
@@ -1,15 +1,36 @@
 
 //resolve-> there will be .then file which will be using return statement but in waitforclick we are not using any .then function
 const puppeteer = require("puppeteer");
-let {email,password} = require('/.secrets';)
-// let email = " ";
-//let password = " ";
-
+let email = "onandon@gmail.com";
+let pass = "cLm@YFyij#-49vE";
 let curTab;
 let browserOpenPromise = puppeteer.launch(
     {
         headless: false,
         defaultViewport: null,
-        args: [""]
+        args: ["--start-maximized"],
+        // executablePath:"//Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     }
-)
+);
+browserOpenPromise //fulfill
+.then(function (browser){
+    console.log("browser is opened");
+    //an array of all open pages inside the browser.
+    //browser.pages=returns an array with all the pages in all browser contexts and all will be stored in allTabspromise.
+    let allTabsPromise = browser.pages();
+    return allTabsPromise;
+})
+.then(function (allTabsArray){
+    curTab=allTabsArray[0];
+    console.log("Access a first tab");
+    let visitLoginPagePromise = curTab.goto("https://www.hackerrank.com/auth/login");
+    return visitLoginPagePromise;
+})
+.then(funtion (data)){
+    console.log("hacker login page is opened now");
+    let emailTypingPromise = curTab.type("#input-1",email);
+    return emailTypingPromise;
+}
+.catch(function(err){
+    console.log(err);
+});
