4237425c082e79bd3f21f29b4374c07b48a5fb1a
Rishabh Verma <rishabhnjr11@gmail.com>
2022-06-14 23:03:39 +0530

    database created

---
+++ Camera_app/db.js
@@ -0,0 +1,15 @@
+//connec to database
+let db;
+let openRequest= indexedDB.open('myDatabase');
+
+opendRequest.addEventListener('success',()=>{
+    console.log('db connnected');
+    db=openRequest.result;
+})
+openRequest.addEventListener('upgradeneeded',()=>{
+    console.log("dataBase upgraded OR dataBase intialized");
+    db=openRequest.result;
+})
+openRequest.addEventListener('error',()=>{
+    console.log("dataBase Error")
+})
